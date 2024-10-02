339f08abe98610c3948b6c68c64ae9ad8ff38a32
Carlos Alberto <47922578+giliover@users.noreply.github.com>
2024-10-02 12:30:26 +0000

    chore: print value from array with for & append values to dinamic array

--- array.go
+++ array.go
@@ -8,4 +8,12 @@ func main() {
 	fmt.Println(arrayStatic)
 	var arrayDinamic []int32
 	fmt.Println(arrayDinamic)
+
+	var arrayDinamic2 []int32
+	arrayDinamic2 = append(arrayDinamic2, 1, 2, 3, 4)
+	fmt.Println(arrayDinamic2)
+
+	for _, v:=range arrayDinamic2 {
+		fmt.Println(v)
+	}
 }
